e69bdfcacf373f118f7a8ddf52dfcc52e5e8dfc5
k2project <kopczkrzy@gmail.com>
2019-05-27 23:00:53 +0100

    Updates

---
+++ precache-manifest.85e76f27e5844f32c0c19eec2b201f9e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "334565d15eb62f20bf85273ffd9436c2",
+    "url": "/fred_mcBride/index.html"
+  },
+  {
+    "revision": "475039e04a19ea1f27b0",
+    "url": "/fred_mcBride/static/css/main.bc1ad4b7.chunk.css"
+  },
+  {
+    "revision": "88ee071bf46fc6b893e6",
+    "url": "/fred_mcBride/static/js/2.7ad91dd1.chunk.js"
+  },
+  {
+    "revision": "475039e04a19ea1f27b0",
+    "url": "/fred_mcBride/static/js/main.b672aa4c.chunk.js"
+  },
+  {
+    "revision": "1bf53fa1c1695f64f85b",
+    "url": "/fred_mcBride/static/js/runtime~main.49ff71ba.js"
+  },
+  {
+    "revision": "eea3cd9b9dabd8f1d454122e3ff207f8",
+    "url": "/fred_mcBride/static/media/bg2.eea3cd9b.jpg"
+  },
+  {
+    "revision": "6f34d3cbabe21018870a92239eb8d738",
+    "url": "/fred_mcBride/static/media/fredMcBride.6f34d3cb.jpeg"
+  },
+  {
+    "revision": "4ffc62d303121efef896c55c92821322",
+    "url": "/fred_mcBride/static/media/logo_transparent.4ffc62d3.png"
+  }
+]);
